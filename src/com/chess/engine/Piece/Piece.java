68a71e0cf63d1656ca53e2d9dda698da208e1ba2
Justin Tran <jhtran023@gmail.com>
2023-04-10 19:46:57 +1000

    Create knight space

--- src/com/chess/engine/Piece/Piece.java
+++ src/com/chess/engine/Piece/Piece.java
@@ -1,10 +1,10 @@
 package com.chess.engine.Piece;
 
+import java.util.Collection;
+
 import com.chess.engine.Alliance;
 import com.chess.engine.board.Board;
 import com.chess.engine.board.Move;
-
-import java.util.List;
 
 public abstract class Piece {
 	
@@ -16,5 +16,9 @@ public abstract class Piece {
 		this.piecePosition = piecePosition;
 	}
 	
-	public abstract List<Move> calculateLegalMoves(final Board board);
+	public Alliance getPieceAlliance() {
+		return this.pieceAlliance;
+	}
+	
+	public abstract Collection<Move> calculateLegalMoves(final Board board);
 }
